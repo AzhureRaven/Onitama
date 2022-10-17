08b72c568970e00b108fd08177ec4acb23138ab6
Kevin Jonathan <kevinjonathan@istts.onmicrosoft.com>
2022-10-17 18:55:35 +0700

    board

--- app/src/main/java/com/example/onitama/MainActivity.kt
+++ app/src/main/java/com/example/onitama/MainActivity.kt
@@ -2,20 +2,30 @@ package com.example.onitama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.example.onitama.card.*
 
 class MainActivity : AppCompatActivity() {
     //lateinit var card:Card
     lateinit var board: Board
+    lateinit var tiles:ArrayList<Button>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startGame()
+        tiles= ArrayList()
+        for(i in 1..5){
+            for(j in 1..5){
+                var tileID = "x$i"+"y$j"
+                val resourcesID = this.resources.getIdentifier(tileID, "id", packageName)
+                tiles.add(findViewById(resourcesID))
+            }
+        }
+        print_to_board()
         //card = HorseCard()
         //Toast.makeText(this, card.toString(), Toast.LENGTH_LONG).show()
     }
-
     fun startGame(){
         val cards = ArrayList<Card>()
         cards.add(TigerCard())
@@ -52,5 +62,27 @@ class MainActivity : AppCompatActivity() {
             cards.removeAt(idx)
         }
         board = Board(b, arrayListOf(c[0],c[1]),arrayListOf(c[2],c[3]),c[4],"P1")
+    }
+    fun print_to_board(){
+        //temp
+
+        val b = arrayListOf<ArrayList<String>>(
+            arrayListOf<String>("N","N","N","N","N","N","N","N","N"),
+            arrayListOf<String>("N","N","N","N","N","N","N","N","N"),
+            arrayListOf<String>("N","N","S2","S2","M2","S2","S2","N","N"),
+            arrayListOf<String>("N","N"," "," "," "," "," ","N","N"),
+            arrayListOf<String>("N","N"," "," "," "," "," ","N","N"),
+            arrayListOf<String>("N","N"," "," "," "," "," ","N","N"),
+            arrayListOf<String>("N","N","S1","S1","M1","S1","S1","N","N"),
+            arrayListOf<String>("N","N","N","N","N","N","N","N","N"),
+            arrayListOf<String>("N","N","N","N","N","N","N","N","N"),
+        )
+        var counter=0
+        for (i in 2..6) {
+            for (j in 2..6) {
+                tiles.get(counter).setText(b[j][i])
+                counter++
+            }
+        }
     }
 }
